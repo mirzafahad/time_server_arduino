41589ada79e06586ca67365ba1b854db9ffdbaaa
Fahad Mirza <fahadmirza8@gmail.com>
2019-05-29 13:23:45 -0500

    Started to convert the server into class!

--- time_server.h
+++ time_server.h
@@ -1,14 +1,14 @@
 /*******************************************************************************
- * File       : timeserver.h
- * Author     : Fahad Mirza (fahad@haxiot.com)
- * Version    : V0.1
+ * File       : time_server.h
+ * Author     : Fahad Mirza (fahadmirza8@gmail.com)
+ * Version    : V1.0
  * Created on : Sep 25, 2017
- * Modified   : Sep 26, 2017
+ * Modified   : May 29, 2019
  * Brief      : Time server driver
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; Copyright (c) 2017 HaxIoT
+ * <h2><center>&copy; Copyright (c) 2017 FahadMirza
  * All rights reserved.</center></h2>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -19,19 +19,19 @@
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 3. Neither the name of HaxIoT nor the names of other
+ * 3. Neither the name of Fahad Mirza nor the names of other
  *    contributors to this software may be used to endorse or promote products
  *    derived from this software without specific written permission.
  * 4. Redistribution and use of this software other than as permitted under
  *    this license is void and will automatically terminate your rights under
  *    this license.
  *
- * THIS SOFTWARE IS PROVIDED BY HAXIOT AND CONTRIBUTORS "AS IS"
+ * THIS SOFTWARE IS PROVIDED BY FAHAD MIRZA AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT
- * SHALL HAXIOT OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * SHALL FAHAD MIRZA OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
@@ -43,50 +43,77 @@
  */
 
  /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef TIMESERVER_H_
-#define TIMESERVER_H_
+#ifndef TIME_SERVER_H
+#define TIME_SERVER_H
 
-#include <stdbool.h>
+
+#if ( ARDUINO >= 100 )
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+
+typedef void (*Callback)(void);
+/**************************************************************************/
+/*! 
+    @brief  Class that stores state and functions for interacting with Time Server
+*/
+/**************************************************************************/
+class Timer_Server
+{
+  public:
+    //Constructor
+    /* 
+     * Timer event initialization.
+     * Param :   interval - Time intervl for callback,
+     *           cb       - Callback function pointer
+     * Return:   none
+     */
+    Timer_Server(uint32_t interval, Callback cb);
+    Timer_Server(void);
+    ~Timer_Server();
+
+    void setCallback(Callback cb);
+    void setInterval(uint32_t interval);
+
+    /* 
+     * Start a particular timer event.
+     * Param :   interval_ms - in milliseconds
+     * Return:   none
+     */
+    void start(void);
+    void start(uint32_t interval);
+
+    /* 
+     * Stop the timer event and remove from the linked list.
+     * Param :   obj - TimerEvent_t object pointer
+     * Return:   none
+     */
+    void stop(void);
+
+    void restart(void);
+
+  private:
+    uint32_t PreviousMillis; // Last Mills value to compare with mills()
+    uint32_t Interval;       // Interval before execute callback()
+    boolean  IsRunning;      // Is the timer currently running?
+    Callback Cb;             // Timer IRQ callback function
+}
+
 
 /************** Private Variables **************/
 // Timer Event struct
 typedef struct sTimerEvent
 {
-  unsigned long previousMillis; // Last Mills value to compare with mills()
-  long Interval;                // Interval before execute callback()
-  bool IsRunning;               // Is the timer currently running?
-  void (*Callback)(void);       // Timer IRQ callback function
   struct sTimerEvent *Next;     // Pointer to the next Timer object.
 }TimerEvent_t;
 
 
-/* 
- * Timer event initialization.
- * Param :   obj - TimerEvent_t object pointer,
- *           callback - Callback function pointer
- * Return:   none
- */
-void Timer_Init(TimerEvent_t *obj, void (*callback)(void));
+
+
 void Timer_PrintAllInstance(void);
 
-
-/* 
- * Start a particular timer event.
- * Param :   obj - TimerEvent_t object pointer,
- *           interval_ms - in milliseconds
- * Return:   none
- */
-void Timer_Start(TimerEvent_t *obj, uint32_t interval_ms);
-void Timer_Start(TimerEvent_t *obj);
-
-void Timer_SetTime(TimerEvent_t *obj, uint32_t interval_ms);
-/* 
- * Stop the timer event and remove from the linked list.
- * Param :   obj - TimerEvent_t object pointer
- * Return:   none
- */
-void Timer_Stop(TimerEvent_t *obj);
-void Timer_Restart(TimerEvent_t *obj);
 
 
 /* 
@@ -94,10 +121,10 @@ void Timer_Restart(TimerEvent_t *obj);
  * Param :   none
  * Return:   none
  */
-void Timer_Handler(void);
+void Time_Server_Handler(void);
 
 
-#endif /* TIMESERVER_H_ */
+#endif /* TIME_SERVER_H */
 
-/************************ (C) COPYRIGHT HaxIoT ************************/
+/************************ (C) COPYRIGHT FahadMirza ************************/
 /*****END OF FILE****/

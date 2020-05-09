613be2789dc1896d44f84ee0954f643cc2b35c2f
fahadmirza <fahadmirza8@gmail.com>
2020-05-09 02:08:54 -0500

    Added more timer event and checked with logic analyzer. seems working;

--- TimeServer.ino
+++ TimeServer.ino
@@ -3,22 +3,52 @@
 
 // Callback functions for timer events
 static void onBlinkTimerEvent(void);
-static TimerEvent BlinkTimer(500, onBlinkTimerEvent, true);
+void D8TimerEvent(void);
+void D9TimerEvent(void);
+void D10TimerEvent(void);
+static TimerEvent BlinkTimer(onBlinkTimerEvent, 250, true);
+static TimerEvent D8Timer(D8TimerEvent, 250, true);
+static TimerEvent D9Timer(D9TimerEvent, 500, true);
+static TimerEvent D10Timer(D10TimerEvent, 1000, true);
+
 
 void setup() 
 {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  
 
   BlinkTimer.start();
+  D8Timer.start();
+  D9Timer.start();
+  D10Timer.start();
 }
 
 void loop() 
 {
-  Timer_Handler();
+  delay(5000);
+  Serial.println("Hi");
 }
 
 void onBlinkTimerEvent(void)
 {
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+void D8TimerEvent(void)
+{
+  digitalWrite(8, !digitalRead(8));
+}
+
+void D9TimerEvent(void)
+{
+  digitalWrite(9, !digitalRead(9));
+}
+
+void D10TimerEvent(void)
+{
+  digitalWrite(10, !digitalRead(10));
 }

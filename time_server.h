80cbaa0b5a6f5b26fda7aa800c867bf23a53969e
fahadmirza <fahadmirza8@gmail.com>
2020-05-09 07:21:48 -0500

    minor cleanup and comments upodated;

--- time_server.h
+++ time_server.h
@@ -8,37 +8,17 @@
 #ifndef TIME_SERVER_H_
 #define TIME_SERVER_H_
 
-#include <stdbool.h>
-
-/*** Public Functions Declarations *************************************/
-
-
-/*********************************************************************** 
- * @brief    Handles the time for all timer event
- * @details  Call this fucnction in loop()
- * @param    None
- * @return   None
- ***********************************************************************/
-void Timer_Handler(void);
-
-/*********************************************************************** 
- * @brief    Print timer instance 
- * @details  Debug function to print all timer instances' address in 
- *           the linked list
- * @param    None
- * @return   None
- ***********************************************************************/
-void Timer_PrintAllInstance(void);
-
+/*** Includes **********************************************************/
+#include <Arduino.h>
 
 // Function pointer typedef. To use inside TimerEvent class
 typedef void (*Callback)(void);
 
-/**************************************************************************/
-/*! 
-    @brief  Class that stores state and functions for interacting with Time Server
-*/
-/**************************************************************************/
+
+/*********************************************************************** 
+ * @brief    Class that stores state and functions for interacting with 
+ *           Time Server
+ ***********************************************************************/
 class TimerEvent
 {
   public:
@@ -48,43 +28,43 @@ class TimerEvent
     boolean  Repeat;         // Is this event needs to be repeated
     Callback Cb;             // Timer event callback function
     
-    /* 
+    /***********************************************************************
      * Timer event initialization.
      * Param :   interval - Time interval for callback,
      *           cb       - Callback function pointer
      *           repeat   - Should the event repeat after executing callback
      * Return:   none
-     */
+     ***********************************************************************/
     TimerEvent(Callback cb, uint32_t interval_ms = 0, boolean repeat = false);
     
-    /*
+    /***********************************************************************
      * Set timer event's interval
      * Param :   interval_ms - interval in milliseconds
      * Return:   none
-     */
+     ***********************************************************************/
     void setInterval(uint32_t interval_ms);
 
-    /* 
+    /***********************************************************************
      * Start a particular timer event.
      * Param :   interval_ms - in milliseconds
      * Return:   none
-     */
+     ***********************************************************************/
     void start(uint32_t interval_ms);
     void start(void);
 
-    /* 
+    /***********************************************************************
      * Stop the timer event and remove from the linked list.
      * Param :   none
      * Return:   none
-     */
+     ***********************************************************************/
     void stop(void);
 
-    /*
+    /***********************************************************************
      * Restart a timer event by stopping the timer event and 
      * then starting it again
      * Param :   none
      * Return:   none
-     */
+     ***********************************************************************/
     void restart(void);
 };
 #endif // TIME_SERVER_H_
